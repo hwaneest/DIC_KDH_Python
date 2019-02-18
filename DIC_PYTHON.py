a2f4e861fa4ca13e7b034e722a275ea4dd5bd13d
hwan141592 <aitwillz@gmail.com>
2019-02-18 23:08:19 +0900

    py dic today

---
+++ DIC_PYTHON.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+#######################################################
+##### This is Python CODES Dictionary             #####
+##### produced by Donghwan Kim                    #####
+##### from Republic of Korea (Seoul, South Korea) #####
+#######################################################
+
+# check working directory
+import os
+os.getcwd()   #current working directory
+
+#print(os.path.realpath(__file__)) #'file'
+#print(os.path.dirname(os.path.realpath(__file__)) ) #directory which has 'file'
+
+
+
+
+
