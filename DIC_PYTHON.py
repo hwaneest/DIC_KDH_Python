78e4bb1282b313f5fc3c115b6e5b6846ae51d919
hwan141592 <aitwillz@gmail.com>
2019-03-12 23:13:52 +0900

    py dic

--- DIC_PYTHON.py
+++ DIC_PYTHON.py
@@ -13,6 +13,13 @@ os.getcwd()   #current working directory
 #print(os.path.realpath(__file__)) #'file'
 #print(os.path.dirname(os.path.realpath(__file__)) ) #directory which has 'file'
 
+#change working directory
+from PIL import Image
+import os
+os.getcwd()   # show current wd
+from os import chdir  # 작업 위치를 바꾸기 위한 헤드
+chdir('C:\\Users\\username\\Desktop') # 작업 위치 변경
+os.getcwd()
 
 
 
